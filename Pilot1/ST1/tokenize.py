23ee9c2c5de352fc1f91d70eba909d344d1211e2
Archit <akvasan2@illinois.edu>
2023-11-28 21:20:50 +0000

    mod tokenize.py

--- Pilot1/ST1/tokenize.py
+++ Pilot1/ST1/tokenize.py
@@ -10,6 +10,16 @@ from typing import List, Optional
 from SmilesPE.tokenizer import SPE_Tokenizer
 from transformers import PreTrainedTokenizer
 
+def load_vocab(vocab_file):
+    """Loads a vocabulary file into a dictionary."""
+    vocab = collections.OrderedDict()
+    with open(vocab_file, "r", encoding="utf-8") as reader:
+        tokens = reader.readlines()
+    for index, token in enumerate(tokens):
+        token = token.rstrip("\n")
+        vocab[token] = index
+    return vocab
+
 class SMILES_SPE_Tokenizer(PreTrainedTokenizer):
     r"""
     Constructs a SMILES tokenizer. Based on SMILES Pair Encoding (https://github.com/XinhaoLi74/SmilesPE).
